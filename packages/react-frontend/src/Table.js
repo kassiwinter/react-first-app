670637f27bdd4b70291e7caf4cafc03f1c7f5067
Kassandra Winter <kassandrawinter@Kassandras-MacBook-Pro.local>
2023-10-09 20:21:09 -0700

    Show ID in table

--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+	<th>ID</th>
         <th>Name</th>
         <th>Job</th>
       </tr>
@@ -12,10 +13,11 @@ function TableHeader() {
   );
 }
 
-function TableBody() {
+function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+	<td>{row.id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
 	<td>
